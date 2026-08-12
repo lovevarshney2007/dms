0275105390639a469938355e6f65090d72a091f2
Love Varshney <lovevarshney7002@gmail.com>
2026-08-12 17:33:41 +0530

    Update default API URL to point to Render deployment

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -2,5 +2,5 @@
 const isLocalhost = typeof window !== "undefined" && window.location.hostname === "localhost";
 
 export const ENV = {
-  API_URL: import.meta.env.VITE_API_URL || (isLocalhost ? 'http://localhost:5051/api' : '/api'),
+  API_URL: import.meta.env.VITE_API_URL || (isLocalhost ? 'http://localhost:5051/api' : 'https://dms-b383.onrender.com/api'),
 };
